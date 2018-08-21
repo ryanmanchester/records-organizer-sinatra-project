209ef260d7839248ca1edfc3b1c19457e7be8c43
Ryan Manchester <ryanm17@gmail.com>
2018-08-21 23:53:05 +0000

    added logged_in? and a log out link

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,27 +1,45 @@
 class UsersController < ApplicationController
 
-  get '/users/home' do
-    if logged_in?
-    @user = User.find_by(username: params["username"])
-    @record = Record.find_by(user_id: session[:user_id])
-    erb :'users/home'
-  else
-    redirect '/hompage'
-  end
-  end
-
   get '/signup' do
     erb :'users/signup'
   end
 
-  post '/signup' do
-    if !params["username"].empty? && !params["email"].empty? && !params["password"].empty?
-      @user = User.create(params)
+  get '/login' do
+    erb :'users/login'
+  end
+
+  get '/users/home' do
+    if logged_in?
+      @user = User.find_by(id: session[:user_id])
+      @record = Record.find_by(user_id: session[:user_id])
+      erb :'users/home'
+  else
+    redirect '/'
+  end
+end
+
+get '/logout' do
+  logout
+  redirect '/'
+end
+
+post '/signup' do
+  if !params["username"].empty? && !params["email"].empty? && !params["password"].empty?
+    @user = User.create(params)
+    session[:user_id] = @user.id
+    redirect 'users/home'
+  else
+    redirect '/signup'
+    end
+  end
+
+  post '/login' do
+    if !logged_in?
+      @user = User.find_by(username: params["username"])
       session[:user_id] = @user.id
-      #binding.pry
-      redirect 'users/home'
+      redirect '/users/home'
     else
-      redirect '/signup'
+      redirect '/login'
     end
   end
 end
