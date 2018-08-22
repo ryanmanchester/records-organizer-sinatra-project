538b3433b6ffff77662dec0f593aaca4b78e74cf
Ryan Manchester <ryanm17@gmail.com>
2018-08-22 00:31:05 +0000

    added password authentication

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,18 +1,26 @@
 class UsersController < ApplicationController
 
   get '/signup' do
+    if logged_in?
+      redirect "users/#{@user.id}"
+    else
     erb :'users/signup'
+  end
   end
 
   get '/login' do
+    if logged_in?
+      redirect "users/#{@user.id}"
+    else
     erb :'users/login'
   end
+  end
 
-  get '/users/home' do
+  get '/users/:id' do
     if logged_in?
-      @user = User.find_by(id: session[:user_id])
+      @user = User.find_by(id: params["id"])
       @record = Record.find_by(user_id: session[:user_id])
-      erb :'users/home'
+      erb :'users/show'
   else
     redirect '/'
   end
@@ -27,17 +35,18 @@ post '/signup' do
   if !params["username"].empty? && !params["email"].empty? && !params["password"].empty?
     @user = User.create(params)
     session[:user_id] = @user.id
-    redirect 'users/home'
+    redirect "users/#{@user.id}"
   else
     redirect '/signup'
     end
   end
 
   post '/login' do
-    if !logged_in?
-      @user = User.find_by(username: params["username"])
+    @user = User.find_by(username: params["username"])
+    
+    if @user && @user.authenticate(params["password"])
       session[:user_id] = @user.id
-      redirect '/users/home'
+      redirect "users/#{@user.id}"
     else
       redirect '/login'
     end
