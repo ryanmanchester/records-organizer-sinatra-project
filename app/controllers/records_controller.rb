8c812cbc1c5768b16ab35072681c8f0fd7da8fdc
Ryan Manchester <ryanm17@gmail.com>
2018-08-23 19:41:48 +0000

    added records views

--- app/controllers/records_controller.rb
+++ app/controllers/records_controller.rb
@@ -1,2 +1,17 @@
 class RecordsController < ApplicationController
+
+  get '/records/new' do
+    erb :'records/new'
+  end
+
+  get '/records/:id' do
+    @record = Record.find_by(user_id: session[:user_id])
+    erb :'/records/show'
+  end
+
+  post '/records' do
+  @record = Record.create(artist: params["artist"], name: params["name"], user_id: session[:user_id])
+  @user = User.find_by(id: session[:user_id])
+  redirect "users/#{@user.id}"
+  end
 end
