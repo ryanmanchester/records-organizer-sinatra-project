ba21c96f191ffa668d6631073ffb2283e3834d71
Ryan Manchester <ryanm17@gmail.com>
2018-08-31 19:35:20 +0000

    added delete, more logged_in?, debugged for 30min video

--- app/controllers/records_controller.rb
+++ app/controllers/records_controller.rb
@@ -4,24 +4,48 @@ class RecordsController < ApplicationController
     erb :'records/new'
   end
 
+  get '/records' do
+    @records = Record.all
+    erb :'/records/index'
+  end
+
   get '/records/:id' do
-    @record = Record.find_by(user_id: session[:user_id])
-    erb :'/records/show'
+    if logged_in?
+      @user = User.find_by(id: session[:user_id])
+      @record = Record.find_by(id: params["id"])
+      
+      erb :'/records/show'
+    else
+      redirect '/'
+    end
   end
 
   get '/records/:id/edit' do
-    @record = Record.find_by(user_id: session[:user_id])
-    erb :'/records/edit'
+    if logged_in?
+      @record = Record.find_by(user_id: session[:user_id])
+      erb :'/records/edit'
+    else
+      redirect '/'
+    end
   end
 
   post '/records' do
-  @record = Record.create(artist: params["artist"], name: params["name"], user_id: session[:user_id])
-  @user = User.find_by(id: session[:user_id])
-  redirect "users/#{@user.id}"
+    @record = Record.create(artist: params["artist"], name: params["name"], user_id: session[:user_id])
+    @user = User.find_by(id: session[:user_id])
+    redirect "/users/#{@user.id}"
   end
 
   patch '/records/:id' do
-  @record = Record.find_by(id: params["id"])
-  @record.update(name: params["name"], artist: params["artist"])
+    @user = User.find_by(id: session[:user_id])
+    @record = Record.find_by(id: params["id"])
+    @record.update(name: params["name"], artist: params["artist"])
+    redirect "/users/#{@user.id}"
+  end
+
+  delete '/records/:id/delete' do
+    @user = User.find_by(id: session[:user_id])
+    @record = Record.find_by(id: params["id"])
+    @record.destroy
+    redirect "/users/#{@user.id}"
   end
 end
