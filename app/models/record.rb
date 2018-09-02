b71165c823019abbace3765dd23d9e600d1c9300
Ryan Manchester <ryanm17@gmail.com>
2018-09-02 21:15:02 +0000

    deleted rack-flash and changed input type to password for password:

--- app/models/record.rb
+++ app/models/record.rb
@@ -1,3 +1,6 @@
 class Record < ActiveRecord::Base
   belongs_to :user
+
+  validates :name, presence: {message: "Please enter the name of the album"}
+  validates :artist, presence: {message: "Please enter the name of the artist"}
 end
