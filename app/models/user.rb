92283558c47a9f87f613c99e3cdd3ed67f0e603b
Ryan Manchester <ryanm17@gmail.com>
2018-09-02 21:23:07 +0000

    added validations and cleaned up User class

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,13 +3,4 @@ class User < ActiveRecord::Base
   has_many :records
 
 
-  def slug
-    self.username.gsub(" ", "-").downcase
-  end
-
-  def self.find_by_slug(slug)
-    self.all.detect do |user|
-      user.slug == slug
-    end
-  end
 end
