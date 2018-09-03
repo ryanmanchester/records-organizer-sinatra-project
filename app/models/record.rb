f845732ae4a87e5727f4a1b32be0de53fe2c64df
Ryan Manchester <ryanm17@gmail.com>
2018-09-03 00:47:00 +0000

    deleted validations from record model, used rack-flash instead

--- app/models/record.rb
+++ app/models/record.rb
@@ -1,6 +1,3 @@
 class Record < ActiveRecord::Base
   belongs_to :user
-
-  validates :name, presence: {message: "Please enter the name of the album"}
-  validates :artist, presence: {message: "Please enter the name of the artist"}
 end
