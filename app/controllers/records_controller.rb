02cfef95855fd05564c786fc933aeb47e49fdca1
Ryan Manchester <ryanm17@gmail.com>
2018-09-25 19:46:19 +0000

    added further validation during review call

--- app/controllers/records_controller.rb
+++ app/controllers/records_controller.rb
@@ -16,20 +16,23 @@ class RecordsController < ApplicationController
   end
 
   get '/records/:id' do
-    if logged_in?
+    @record = Record.find_by(id: params["id"])
+    
+    if logged_in? && @record.user_id == @user.id
       @user = User.find_by(id: session[:user_id])
-      @record = Record.find_by(id: params["id"])
 
       erb :'/records/show'
     else
+
       flash[:message] = "Please log in or sign up"
       redirect '/records'
     end
   end
 
   get '/records/:id/edit' do
-    if logged_in?
       @record = Record.find_by(id: params["id"])
+    if logged_in? && @record.user_id == @user.id
+
       erb :'/records/edit'
     else
       flash[:message] = "You must be logged in to edit your record collection"
@@ -53,18 +56,28 @@ class RecordsController < ApplicationController
   patch '/records/:id' do
     @user = User.find_by(id: session[:user_id])
     @record = Record.find_by(id: params["id"])
+
+    if logged_in? && @record.user_id == @user.id
     @record.update(name: params["name"], artist: params["artist"])
 
     flash[:message] = "Record sucessfully updated"
     redirect "/users/#{@user.id}"
+   else
+    flash[:message] = "You must own a record to make changes"
+    redirect "/users/#{@user.id}"
+   end
   end
 
   delete '/records/:id/delete' do
     @user = User.find_by(id: session[:user_id])
     @record = Record.find_by(id: params["id"])
+    if logged_in? && @record.user_id == @user.id
     @record.destroy
 
     flash[:message] = "Record sucessfully deleted"
+    redirect "/users/#{@user.id}"
+  else
+    flash[:message] = "You must own a record to delete it."
     redirect "/users/#{@user.id}"
   end
 end
