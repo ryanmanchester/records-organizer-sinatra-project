858379549b2017d097375aedccef1d6df2f60eb4
github username <github email address>
2020-02-24 13:10:37 -0500

    added bootstrap styling and images for album covers

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_16_044336) do
+ActiveRecord::Schema.define(version: 2020_02_24_175559) do
 
   create_table "records", force: :cascade do |t|
     t.string "name"
     t.string "artist"
     t.integer "user_id"
+    t.string "image_url"
   end
 
   create_table "users", force: :cascade do |t|
