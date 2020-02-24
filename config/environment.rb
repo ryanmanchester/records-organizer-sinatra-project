98cf294ff9f41b87718d54e62232347a51de3eef
github username <github email address>
2020-02-24 18:42:23 -0500

    postresql db for heroku added

--- config/environment.rb
+++ config/environment.rb
@@ -3,9 +3,17 @@ ENV['SINATRA_ENV'] ||= "development"
 require 'bundler/setup'
 Bundler.require(:default, ENV['SINATRA_ENV'])
 
+configure :production do
+   db = URI.parse(ENV['DATABASE_URL'] || 'postgres://localhost/mydb')
+
 ActiveRecord::Base.establish_connection(
-  :adapter => "sqlite3",
-  :database => "db/#{ENV['SINATRA_ENV']}.sqlite"
+  :adapter => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
+     :host     => db.host,
+     :username => db.user,
+     :password => db.password,
+     :database => db.path[1..-1],
+     :encoding => 'utf8'
 )
+end
 
 require_all 'app'
